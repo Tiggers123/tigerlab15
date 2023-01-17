e388be5287c264f21ad4b25ff9719b7e1d72c2bf
tigerlab <tiggers12@hotmail.com>
2023-01-17 18:53:55 +0700

    qwe

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,35 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool m[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << m[i][j] << " ";
+		} 
+		cout << "\n";
+	}
+}
+void inputMatrix(double m[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": "; 
+		for(int j = 0; j < N; j++){
+			cin >> m[i][j];
+		}
+	}
+}
+void findLocalMax(const double m[][N], bool b[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(i == 0 || i == N-1 ||j == 0 || j == N-1){
+				b[i][j] = 0;
+			}
+			else if (m[i][j] >= m[i-1][j]&&m[i][j] >= m[i][j-1]&&m[i][j] >= m[i][j+1]&&m[i][j] >= m[i+1][j] )
+			{
+				b[i][j] = 1;
+			}
+			else b[i][j]=0;
+		}
+	} 
+}
+
